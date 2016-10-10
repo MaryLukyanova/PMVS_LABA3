74d73cc7873a483117dd63235f39d7c9d3604369
toyer <bobbkrist@gmail.com>
2016-10-10 15:18:06 +0200

    functional for creating process

--- server.c
+++ server.c
@@ -12,6 +12,41 @@ typedef struct _params{
 	int socket;
 }params;
 
+void *compute(void* arg){
+	char buffer[256];
+	int n;
+	bzero(buffer,256);
+	params parameter = *( (params*) arg );
+	n = read(parameter.socket,buffer,255);
+	if (n < 0) {
+		perror("ERROR reading from socket");
+	}
+	else
+	{
+		FILE *fp;
+		int status;
+		char path[255];
+		fp = popen(buffer, "r");
+		if (fp == NULL) {
+			perror("ERROR executing");
+		}
+		bzero(path,256);
+		while (fgets(path, 100, fp) != NULL) {
+			n = write(parameter.socket,path,strlen(path));
+			if (n < 0) {
+				perror("ERROR writing");
+			}
+			bzero(path,256);
+		}
+		strcpy(path, "N");
+		n = write(parameter.socket, path, strlen(path));
+		if (n<0) {
+			perror("ERROR writing");
+		}
+	}
+	close(parameter.socket);
+}
+
 int main()
 {
 	int sockfd, newsockfd, portno;
@@ -35,6 +70,31 @@ int main()
 	}
 	listen(sockfd,5);
 	clilen = sizeof(cli_addr);
+	while (1) {
+			int new_socket=0;
+			if ((new_socket = accept(sockfd, 
+				(struct sockaddr *) &cli_addr, &clilen))<0) {
+					perror("accept");
+					exit(EXIT_FAILURE);
+			}
+			else {
+				params parameter;
+				parameter.socket = new_socket;
+			#ifdef PROCESS
+				pid_t childId = 0;
+				if ((childId=fork())==-1) {
+					perror("fork");
+					exit(1);
+				}
+				if (childId == 0) {
+					printf("Started processing request\n");
+					compute(&parameter);
+					printf("Finished processing request\n");
+					return 0;
+				}
+			#endif
+			}
+		}
 	
 	close(sockfd);
 	return 0;
